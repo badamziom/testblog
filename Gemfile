442ce33316cd2e2106898d7d23857e268b84f21f
Adam <aszulist@o2.pl>
2015-08-16 13:07:03 +0200

    Added image to posts

--- Gemfile
+++ Gemfile
@@ -51,5 +51,8 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+
+  #Gem to saving images in forms
+  gem "paperclip", "~> 4.2"
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816084633) do
+ActiveRecord::Schema.define(version: 20150816102653) do
 
   create_table "achievements", force: :cascade do |t|
     t.string   "name"
@@ -47,9 +47,13 @@ ActiveRecord::Schema.define(version: 20150816084633) do
     t.integer  "views"
     t.integer  "score"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "title"
+    t.string   "image_file_name"
+    t.integer  "image_file_size"
+    t.string   "image_content_type"
+    t.datetime "image_updated_at"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
