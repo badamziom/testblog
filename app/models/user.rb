bfb5e248d74d9e7d4fe1b4b572016cf43f77d4ef
Wojtek Pruszak <setzo94@gmail.com>
2015-08-16 14:37:34 +0200

    User registeration form

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ActiveRecord::Base
   has_many :user_to_achievements
   has_many :achievements, :through => :user_to_achievements
 
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
 end
