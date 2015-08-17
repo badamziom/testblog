4ae2e7be18e23155983c3c45eaef8793a32616e2
Adam <aszulist@o2.pl>
2015-08-17 06:43:21 +0200

    Added comments in show post

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -3,4 +3,7 @@ class Achievement < ActiveRecord::Base
   has_many :user_to_achievements
   has_many :users, :through => :user_to_achievements
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150816102653) do
+ActiveRecord::Schema.define(version: 20150816114323) do
 
   create_table "achievements", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
